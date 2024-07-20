f0f01ddeae5289c4c4881005927fc73be5a65aa0
GitHub Action <action@github.com>
2024-07-20 14:23:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/hamna9876/commentspluginintellij/MyBundle.kt
+++ src/main/kotlin/com/github/hamna9876/commentspluginintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hamna9876.commentspluginintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hamna9876/commentspluginintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hamna9876/commentspluginintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hamna9876.commentspluginintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
